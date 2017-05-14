c4350541c07d115ac6d0131809f8e86ebc04af6a
Robert Wayne <robertjwayne@gmail.com>
2017-05-14 14:23:59 -0400

    Build form to create a new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,38 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class SongsController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  use Rack::Flash
+  enable :sessions
   set :views, Proc.new { File.join(root, "../views/songs/") }
+
+
 
   get '/songs' do
     @songs = Song.all
     erb :index
+  end
+
+  post '/songs' do
+    @song = Song.create(name: params[:song][:name])
+
+    unless params[:artist][:name].empty?
+      @artist = Artist.find_or_create_by(name: params[:artist][:name])
+      @artist.songs << @song
+    end
+
+    params[:genres].each do |genre_id|
+      @song.genres << Genre.find(genre_id)
+    end
+
+    flash[:message] = "Successfully created song."
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :new
   end
 
   get '/songs/:slug' do
--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,9 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-configure :development do
+configure do
   set :database, 'sqlite3:db/database.db'
+  enable :sessions
 end
 
 ActiveRecord::Base.establish_connection(
